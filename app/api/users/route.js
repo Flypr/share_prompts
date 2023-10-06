bd25725870353beb12e58d2c2ea1e8d1d6c4ea54
Filip Mihocas <filip.mihocas@gmail.com>
2023-10-06 14:18:52 +0300

    api test

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,3 +1,13 @@
 export async function GET(request) {
-    return new Response('Hello, Next.js!')
+    // Handle GET request for /api/users
+    // Retrieve the users from the database or any data source
+    const users = [
+        { id: 1, name: 'John' },
+        { id: 2, name: 'Jane' },
+        { id: 3, name: 'Bob' }
+    ];
+
+    // Send users as a response
+    return new Response(JSON.stringify(users));
 }
+
