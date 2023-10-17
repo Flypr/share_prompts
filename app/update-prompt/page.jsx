e9239d2d65bd2a00569eeadde7a9e15bf695078d
Filip Mihocas <filip.mihocas@gmail.com>
2023-10-17 13:59:18 +0300

    mvp completed

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,14 +1,14 @@
 "use client";
 
-import { useState } from 'react';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
+import { useState, useEffect } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 import Form from '@components/Form';
 
 const EditPropmt = () => {
     const router = useRouter();
-    const { data: session } = useSession();
+    const searchParams = useSearchParams();
+    const promptId = searchParams.get('id');
 
     const [submitting, setSubmitting] = useState(false);
     const [post, setPost] = useState({
@@ -17,19 +17,30 @@ const EditPropmt = () => {
     });
 
     useEffect(() => {
-        
+        const getPromptDetails = async () => {
+            const response = await fetch(`/api/prompt/${promptId}`);
+            const data = await response.json();
+
+            setPost({
+                prompt: data.prompt,
+                tag: data.tag,
+            })
+        }
+
+        if(promptId) getPromptDetails();
     }, [promptId])
 
-    const createPropmt = async (e) => {
+    const updatePropmt = async (e) => {
         e.preventDefault();
         setSubmitting(true);
 
+        if(!promptId) return alert('Prompt ID not found')
+
         try {
-            const response = await fetch('/api/prompt/new', {
-               method: 'POST',
+            const response = await fetch(`/api/prompt/${promptId}`, {
+               method: 'PATCH',
                body: JSON.stringify({
                 prompt: post.prompt,
-                userId: session?.user.id,
                 tag: post.tag,
                })
             })
@@ -46,11 +57,11 @@ const EditPropmt = () => {
 
     return (
         <Form 
-            type="Create"
+            type="Edit"
             post={post}
             setPost={setPost}
             submitting={submitting}
-            handleSubmit={createPropmt}
+            handleSubmit={updatePropmt}
         />
     )
 }
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -43,8 +43,8 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         <div className='copy_btn' onClick={handleCopy}>
           <Image 
             src={copied === post.prompt
-              ? 'assets/images/tick.svg'
-              : 'assets/images/copy.svg'
+              ? 'assets/icons/tick.svg'
+              : 'assets/icons/copy.svg'
             }
             width={12}
             height={12}
@@ -60,7 +60,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         className='font-inter text-sm blue_gradient cursor_pointer'
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
-        {post.tag}
+        #{post.tag}
       </p>
 
       {session?.user.id === post.creator._id && pathName === '/profile' && (
